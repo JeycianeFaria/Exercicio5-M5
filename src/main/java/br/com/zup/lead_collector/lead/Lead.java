cf2c7db5c91622b0ff76dfe1ba525f60dd4ca64e
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-11-19 07:32:00 -0300

    Criar entidade Lead

---
+++ src/main/java/br/com/zup/lead_collector/lead/Lead.java
@@ -0,0 +1,25 @@
+package br.com.zup.lead_collector.lead;
+
+import br.com.zup.lead_collector.produto.Produto;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "leads")
+@NoArgsConstructor
+@Getter
+@Setter
+public class Lead {
+
+    @Id
+    private String email;
+    @Column(columnDefinition = "VARCHAR(100) DEFAULT 'Não informado'")
+    private String nome;
+    @ManyToMany(cascade = {CascadeType.PERSIST,CascadeType.MERGE})
+    private List<Produto> produtos;
+
+}
