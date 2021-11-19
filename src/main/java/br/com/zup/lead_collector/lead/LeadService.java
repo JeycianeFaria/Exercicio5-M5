bcb9cce48e44f62b594f4554f70b750974df4785
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-11-19 11:47:22 -0300

    Commitão da solução

--- src/main/java/br/com/zup/lead_collector/lead/LeadService.java
+++ src/main/java/br/com/zup/lead_collector/lead/LeadService.java
@@ -1,21 +1,64 @@
 package br.com.zup.lead_collector.lead;
 
+import br.com.zup.lead_collector.produto.Produto;
+import br.com.zup.lead_collector.produto.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class LeadService {
 
     @Autowired
     LeadRepository leadRepository;
+    @Autowired
+    ProdutoRepository produtoRepository;
 
-    public Lead salvarLead(Lead leadRecebido){
-        return leadRepository.save(leadRecebido);
+    public List<Produto> atualizarProdutos(List<Produto> produtosRecebidos) {
+        List<Produto> listaAtualizada = new ArrayList<>();
+
+        for (Produto referencia : produtosRecebidos) {
+            if (produtoRepository.existsById(referencia.getNomeProduto())) {
+                listaAtualizada.add(produtoRepository.findByNomeProduto(referencia.getNomeProduto()));
+            } else {
+                listaAtualizada.add(referencia);
+            }
+        }
+
+        return listaAtualizada;
     }
 
-    public List<Lead> buscarLeadsPorProduto(String nomeProduto){
+
+    public Lead salvarLead(Lead leadRecebido) {
+        List<Produto> produtos = atualizarProdutos(leadRecebido.getProdutos());
+        leadRecebido.setProdutos(produtos);
+        Lead lead = buscarLead(leadRecebido);
+
+        return leadRepository.save(lead);
+    }
+
+
+    public Lead buscarLead(Lead lead){
+
+        Optional<Lead> leadOptional = leadRepository.findById(lead.getEmail());
+
+        if (leadOptional.isPresent()){
+            for (Produto referencia: lead.getProdutos()){
+                if (!leadOptional.get().getProdutos().contains(referencia)){
+                    leadOptional.get().getProdutos().add(referencia);
+                }
+            }
+
+            return leadOptional.get();
+        }
+
+        return lead;
+    }
+
+    public List<Lead> buscarLeadsPorProduto(String nomeProduto) {
         return leadRepository.findAllByProdutosNomeProduto(nomeProduto);
     }
 }
--- src/main/java/br/com/zup/lead_collector/produto/ProdutoRepository.java
+++ src/main/java/br/com/zup/lead_collector/produto/ProdutoRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface ProdutoRepository extends CrudRepository<Produto,String> {
 
+    Produto findByNomeProduto(String nomeProduto);
+
 }
