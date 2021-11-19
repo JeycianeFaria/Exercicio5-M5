a7c3bdd196d035df12db9e20c86d451b6d48a477
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-11-19 07:20:28 -0300

    Primeiro commit

---
+++ src/test/java/br/com/zup/lead_collector/LeadCollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.lead_collector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeadCollectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
