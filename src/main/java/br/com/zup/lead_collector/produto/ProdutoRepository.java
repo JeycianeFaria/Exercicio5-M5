e3f005c45e664556f2998e603329e42ac212e0f7
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-11-19 07:31:31 -0300

    Criar repository para Produto

---
+++ src/main/java/br/com/zup/lead_collector/produto/ProdutoRepository.java
@@ -0,0 +1,7 @@
+package br.com.zup.lead_collector.produto;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProdutoRepository extends CrudRepository<Produto,String> {
+
+}
